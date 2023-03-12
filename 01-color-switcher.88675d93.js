7eb50791f85fe7b988c84947bdfbf97a2cb157c1
ultrasoundcode <0x.ultrasound@gmail.com>
2023-03-12 14:06:19 +0000

    Deploying to gh-pages from @ ultrasoundcode/goit-js-hw-09@334368fb3f95c77fdb9e7d5eb60653af8e0d9e8f 🚀

---
+++ 01-color-switcher.88675d93.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]").addEventListener("click",(function(){e||(e=setInterval(n,1e3),t.disabled=!0)}));document.querySelector("[data-stop]").addEventListener("click",(function(){clearInterval(e),e=null,t.disabled=!1}));let e=null;function n(){const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}
+//# sourceMappingURL=01-color-switcher.88675d93.js.map
