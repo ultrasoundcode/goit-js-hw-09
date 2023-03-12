334368fb3f95c77fdb9e7d5eb60653af8e0d9e8f
Askhat Anarbayev <0x.ultrasound@gmail.com>
2023-03-12 20:05:50 +0600

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,14 +1,29 @@
+const startBtn = document
+  .querySelector('[data-start]')
+  .addEventListener('click', onStartBtnClick);
+const stopBtn = document
+  .querySelector('[data-stop]')
+  .addEventListener('click', onStopBtnClick);
+let intervalId = null;
+
+function setRandomColor() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+}
+
+function onStartBtnClick() {
+  if (!intervalId) {
+    intervalId = setInterval(setRandomColor, 1000);
+    startBtn.disabled = true;
+  }
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+  intervalId = null;
+  startBtn.disabled = false;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-document.querySelector('[data-start]').addEventListener('click', function () {
-  let interval = setInterval(function () {
-    document.body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
-});
-
-document.querySelector('[data-stop]').addEventListener('click', function () {
-  clearInterval(interval);
-  this.disabled = true;
-});
